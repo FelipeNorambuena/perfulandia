f7cd7bc914a6fc4d2eb9ca8017fbed6ea28fc077
FelipeNorambuena <fel.norambuena@duocuc.cl>
2025-06-08 00:10:03 -0400

    Actualiza el volcado SQL para incluir nuevas tablas y datos, y ajusta la estructura de varias tablas existentes.

--- perfulandia.sql
+++ perfulandia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-06-2025 a las 23:43:50
+-- Tiempo de generación: 08-06-2025 a las 06:09:14
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -47,6 +47,19 @@ INSERT INTO `clientes` (`id_cliente`, `id_usuario`, `nombre_completo`, `rut`, `d
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `cupones`
+--
+
+CREATE TABLE `cupones` (
+  `id_descuento` int(10) NOT NULL,
+  `codigo` varchar(30) DEFAULT NULL,
+  `descuento` int(5) NOT NULL,
+  `valido_hasta` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `detalleventa`
 --
 
@@ -55,7 +68,7 @@ CREATE TABLE `detalleventa` (
   `id_venta` int(11) DEFAULT NULL,
   `id_producto` int(11) DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL,
-  `precio_unitario` decimal(10,2) DEFAULT NULL
+  `precio_unitario` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -76,6 +89,30 @@ CREATE TABLE `envios` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `historial_clientes`
+--
+
+CREATE TABLE `historial_clientes` (
+  `id_historial` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial_compras`
+--
+
+CREATE TABLE `historial_compras` (
+  `id_historial` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_vendedor` int(11) NOT NULL,
+  `id_venta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `inventario`
 --
 
@@ -85,6 +122,37 @@ CREATE TABLE `inventario` (
   `stock_disponible` int(11) DEFAULT NULL,
   `ubicacion_bodega` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inventario`
+--
+
+INSERT INTO `inventario` (`id_inventario`, `id_producto`, `stock_disponible`, `ubicacion_bodega`) VALUES
+(1, 4, 500, 'Concepcion'),
+(2, 5, 500, 'Concepcion'),
+(3, 6, 350, 'Santiago'),
+(4, 7, 400, 'Viña del Mar'),
+(5, 8, 300, 'Concepcion'),
+(6, 9, 450, 'Santiago'),
+(7, 10, 380, 'Viña del Mar'),
+(8, 11, 420, 'Concepcion'),
+(9, 12, 500, 'Santiago'),
+(10, 13, 350, 'Viña del Mar'),
+(11, 14, 370, 'Concepcion'),
+(12, 15, 420, 'Santiago'),
+(13, 16, 390, 'Viña del Mar'),
+(14, 17, 330, 'Concepcion'),
+(15, 18, 460, 'Santiago'),
+(16, 19, 410, 'Viña del Mar'),
+(17, 20, 490, 'Concepcion'),
+(18, 21, 320, 'Santiago'),
+(19, 22, 350, 'Viña del Mar'),
+(20, 23, 270, 'Concepcion'),
+(21, 24, 310, 'Santiago'),
+(22, 25, 290, 'Viña del Mar'),
+(23, 26, 230, 'Concepcion'),
+(24, 27, 340, 'Santiago'),
+(25, 28, 280, 'Viña del Mar');
 
 -- --------------------------------------------------------
 
@@ -106,7 +174,31 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id_producto`, `nombre`, `descripcion`, `precio_unitario`, `categoria`, `activo`) VALUES
-(3, 'Laptop Gamer', 'Laptop con procesador Intel i7 y 16GB RAM', 1199900, 'Tecnología', 1);
+(4, 'Dolce & Gabbana Light Blue Pour Homme EDT', 'Fragancia masculina fresca y vibrante, inspirada en el espíritu del Mediterráneo. Combina notas cítricas y amaderadas para una sensación limpia, energética y sofisticada. Ideal para el uso diario en climas cálidos o actividades informales.', 61990, 'Fragancia masculina', 1),
+(5, 'Dior Sauvage Parfum', 'Aroma masculino intenso y sofisticado, con notas de bergamota, vainilla y madera.', 89990, 'Fragancia masculina', 1),
+(6, 'Acqua Di Gio Profumo', 'Fragancia elegante con notas marinas, incienso y pachuli. Ideal para la noche.', 84990, 'Fragancia masculina', 1),
+(7, 'Bleu de Chanel', 'Notas amaderadas y cítricas. Aroma fresco y masculino para uso diario o de oficina.', 79990, 'Fragancia masculina', 1),
+(8, 'Invictus Paco Rabanne', 'Notas frescas con toques dulces y amaderados. Perfume moderno y juvenil.', 69990, 'Fragancia masculina', 1),
+(9, 'Dolce & Gabbana The One for Men', 'Fragancia cálida y especiada. Elegancia clásica para hombres sofisticados.', 65990, 'Fragancia masculina', 1),
+(10, 'Chanel Coco Mademoiselle', 'Elegante fragancia femenina con notas florales y toques de ámbar y vainilla.', 94990, 'Fragancia femenina', 1),
+(11, 'Lancôme La Vie Est Belle', 'Dulce y floral, esta fragancia es sinónimo de feminidad y libertad.', 84990, 'Fragancia femenina', 1),
+(12, 'Carolina Herrera Good Girl', 'Seductora mezcla de flores blancas y cacao. Sensual y poderosa.', 81990, 'Fragancia femenina', 1),
+(13, 'YSL Black Opium', 'Aroma adictivo de café, vainilla y flores blancas. Ideal para la noche.', 78990, 'Fragancia femenina', 1),
+(14, 'Escada Flor del Sol', 'Fragancia veraniega y frutal, con notas de granada y cítricos tropicales.', 59990, 'Fragancia femenina', 1),
+(15, 'Tom Ford Neroli Portofino', 'Fragancia cítrica y sofisticada, ideal para cualquier género. Inspirada en la costa italiana.', 112990, 'Fragancia unisex', 1),
+(16, 'CK One', 'Clásica fragancia fresca, ligera y unisex. Perfecta para uso diario.', 48990, 'Fragancia unisex', 1),
+(17, 'Maison Margiela Lazy Sunday Morning', 'Aroma suave a flores blancas y almizcle, unisex y reconfortante.', 87990, 'Fragancia unisex', 1),
+(18, 'Byredo Blanche', 'Fragancia minimalista, limpia y floral. Ideal para quienes buscan sutileza.', 98990, 'Fragancia unisex', 1),
+(19, 'Acqua di Parma Colonia', 'Cítrica, ligera y clásica. Unisex, con gran frescura.', 65990, 'Fragancia unisex', 1),
+(20, 'Victoria’s Secret Pure Seduction', 'Bruma corporal frutal con ciruela roja y fresia. Ligera y juvenil.', 10990, 'Body Mist', 1),
+(21, 'Bath & Body Works Gingham', 'Bruma fresca con notas cítricas y florales. Ideal para después de la ducha.', 17990, 'Body Mist', 1),
+(22, 'Victoria’s Secret Love Spell', 'Bruma con mezcla de cereza, durazno y flor de azahar. Dulce y romántica.', 19990, 'Body Mist', 1),
+(23, 'Bath & Body Works Into the Night', 'Bruma intensa con grosellas negras y ámbar. Ideal para salidas nocturnas.', 18990, 'Body Mist', 1),
+(24, 'Ariana Grande Cloud Body Mist', 'Aroma ligero de crema batida, coco y lavanda. Versión bruma del famoso perfume.', 17990, 'Body Mist', 1),
+(25, 'Miniatura Dior Homme Sport', 'Miniatura de 10 ml ideal para viajes. Notas cítricas y frescas con toque especiado.', 11990, 'Miniatura', 1),
+(26, 'Miniatura Versace Eros', 'Tamaño compacto de 5 ml. Fragancia intensa, masculina y elegante.', 9990, 'Miniatura', 1),
+(27, 'Set de Regalo Calvin Klein Women', 'Incluye perfume 50 ml, crema corporal y miniatura. Ideal para regalar.', 65990, 'Set de regalo', 1),
+(28, 'Set de Regalo Jean Paul Gaultier Scandal', 'Estuche con perfume, loción corporal y miniatura. Edición especial.', 75990, 'Set de regalo', 1);
 
 -- --------------------------------------------------------
 
@@ -116,10 +208,10 @@ INSERT INTO `productos` (`id_producto`, `nombre`, `descripcion`, `precio_unitari
 
 CREATE TABLE `reportes` (
   `id_reporte` int(11) NOT NULL,
-  `tipo_reporte` varchar(100) DEFAULT NULL,
-  `fecha_generacion` date DEFAULT NULL,
-  `descripcion` text DEFAULT NULL,
-  `json_datos` text DEFAULT NULL
+  `tipo_reporte` varchar(255) DEFAULT NULL,
+  `fecha_generacion` varchar(255) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `json_datos` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -131,9 +223,9 @@ CREATE TABLE `reportes` (
 CREATE TABLE `soporte` (
   `id_ticket` int(11) NOT NULL,
   `id_usuario` int(11) DEFAULT NULL,
-  `tipo_ticket` varchar(100) DEFAULT NULL,
-  `descripcion` text DEFAULT NULL,
-  `estado` varchar(30) DEFAULT NULL,
+  `tipo_ticket` varchar(255) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `estado` varchar(255) DEFAULT NULL,
   `fecha_creacion` date DEFAULT NULL,
   `fecha_resolucion` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -175,15 +267,16 @@ CREATE TABLE `vendedores` (
   `id_usuario` int(11) DEFAULT NULL,
   `nombre_completo` varchar(255) DEFAULT NULL,
   `rut` varchar(255) DEFAULT NULL,
-  `area_ventas` varchar(255) DEFAULT NULL
+  `area_ventas` varchar(255) DEFAULT NULL,
+  `meta` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `vendedores`
 --
 
-INSERT INTO `vendedores` (`id_vendedor`, `id_usuario`, `nombre_completo`, `rut`, `area_ventas`) VALUES
-(4, 12, 'Camila Pérez', '11.223.334-5', 'Región Metropolitana');
+INSERT INTO `vendedores` (`id_vendedor`, `id_usuario`, `nombre_completo`, `rut`, `area_ventas`, `meta`) VALUES
+(4, 12, 'Camila Pérez', '11.223.334-5', 'Región Metropolitana', 0);
 
 -- --------------------------------------------------------
 
@@ -195,7 +288,7 @@ CREATE TABLE `ventas` (
   `id_venta` int(11) NOT NULL,
   `id_cliente` int(11) DEFAULT NULL,
   `id_vendedor` int(11) DEFAULT NULL,
-  `fecha_venta` date DEFAULT NULL
+  `fecha_venta` datetime(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -208,6 +301,12 @@ CREATE TABLE `ventas` (
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id_cliente`),
   ADD KEY `clientes_ibfk_1` (`id_usuario`);
+
+--
+-- Indices de la tabla `cupones`
+--
+ALTER TABLE `cupones`
+  ADD PRIMARY KEY (`id_descuento`);
 
 --
 -- Indices de la tabla `detalleventa`
@@ -223,6 +322,22 @@ ALTER TABLE `detalleventa`
 ALTER TABLE `envios`
   ADD PRIMARY KEY (`id_envio`),
   ADD KEY `envios_ibfk_1` (`id_venta`);
+
+--
+-- Indices de la tabla `historial_clientes`
+--
+ALTER TABLE `historial_clientes`
+  ADD PRIMARY KEY (`id_historial`);
+
+--
+-- Indices de la tabla `historial_compras`
+--
+ALTER TABLE `historial_compras`
+  ADD PRIMARY KEY (`id_historial`),
+  ADD KEY `idx_historial_cliente` (`id_cliente`),
+  ADD KEY `idx_historial_producto` (`id_producto`),
+  ADD KEY `idx_historial_vendedor` (`id_vendedor`),
+  ADD KEY `idx_historial_venta` (`id_venta`);
 
 --
 -- Indices de la tabla `inventario`
@@ -282,10 +397,16 @@ ALTER TABLE `clientes`
   MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT de la tabla `cupones`
+--
+ALTER TABLE `cupones`
+  MODIFY `id_descuento` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `detalleventa`
 --
 ALTER TABLE `detalleventa`
-  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `envios`
@@ -294,16 +415,28 @@ ALTER TABLE `envios`
   MODIFY `id_envio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT de la tabla `historial_clientes`
+--
+ALTER TABLE `historial_clientes`
+  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `historial_compras`
+--
+ALTER TABLE `historial_compras`
+  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `inventario`
 --
 ALTER TABLE `inventario`
-  MODIFY `id_inventario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_inventario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de la tabla `reportes`
@@ -357,6 +490,15 @@ ALTER TABLE `detalleventa`
 --
 ALTER TABLE `envios`
   ADD CONSTRAINT `envios_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `ventas` (`id_venta`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `historial_compras`
+--
+ALTER TABLE `historial_compras`
+  ADD CONSTRAINT `fk_historial_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_historial_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_producto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_historial_vendedor` FOREIGN KEY (`id_vendedor`) REFERENCES `vendedores` (`id_vendedor`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_historial_venta` FOREIGN KEY (`id_venta`) REFERENCES `ventas` (`id_venta`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `inventario`
